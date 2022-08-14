7546305441aeeac66fd02273d09b96add4e277fb
Sstandby <standbyy9@gmail.com>
2022-08-14 17:03:37 -0500

    change: version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as stream:
 
 setup(
     name = "kyo",
-    version = "1.0.3",
+    version = "1.1.0",
     url = "https://github.com/Sstandby/kyo",
     download_url = "https://github.com/Sstandby/kyo/tarball/master",
     license = "MIT",
